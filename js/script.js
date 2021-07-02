eca4562c2c350cfa6c9792983c4a8f6340f15da5
Francesco <francescococlite@gmail.com>
2021-07-02 15:56:01 +0200

    errore strano

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 var app = new Vue({
     el: '#app',
     data: {
-     
+      array : [],
+    },
+    mounted (){
+      for (let i = 0; i < 10; i++){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(element =>{
+         this.array.push(element.data.response) 
+        })
+      }
     }
   })
-  
